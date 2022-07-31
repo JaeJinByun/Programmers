1ce2fb3392de97f7c93be85e36aa80a3c5a059da
JaeJinByun <byunjin11@naver.com>
2022-07-31 22:02:08 +0900

    Level2 / H-index / 조건부 포문

---
+++ Programmers/src/level2/H_index/Main.java
@@ -0,0 +1,30 @@
+package level2.H_index;
+
+import java.util.Arrays;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int[] arr = {3,0,6,1,4,8,9};
+		System.out.println(solution( arr ) );
+	}
+	
+	public static int solution(int[] citations) {
+		int answer = 0;
+		
+		Arrays.sort(citations);
+		
+		for(int i=0; i<citations.length; i++) {
+			int compare_num = citations.length - i;
+			
+			if(citations[i] >= compare_num) {
+				answer = compare_num;
+				break;
+			}
+		}
+		
+		
+		return answer;
+	}
+	
+}
