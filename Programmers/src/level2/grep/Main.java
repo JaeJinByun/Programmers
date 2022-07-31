883eade6fffa9b37a24ca329d1f0ef93e6745cc2
JaeJinByun <byunjin11@naver.com>
2022-07-31 22:01:34 +0900

    Level2 / Grep / 소수판별 /

---
+++ Programmers/src/level2/grep/Main.java
@@ -0,0 +1,34 @@
+package level2.grep;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		
+	}
+	 public static int[] solution(long begin, long end) {
+	       
+			int[] answer = new int[(int) (end - begin) + 1];	
+			// begin ~ end 사이의 값 판별 
+			// 인덱스의 번호는 최대 공약수 
+			// 조건 소수일시 1 
+			
+			for(long i=begin; i<=end; i++) {
+				answer[(int) (i-begin)] = (int)isPrime(i);
+			}			
+			 return answer;
+		}	
+		public static long isPrime(long number) {
+	        if(number == 1) {
+	            return 0;
+	        }
+	        
+			for(int i=2; i <= (int)Math.sqrt(number); i++) {
+				if(number % i == 0) {
+					return number/i;
+				}
+			}
+			// 소수 
+			return 1;
+		}
+}
