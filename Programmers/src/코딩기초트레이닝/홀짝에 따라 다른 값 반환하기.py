3f514bd7954770079f3c95f70c4464580564faf0
wlswo <byunjin11@naver.com>
2023-07-28 11:30:46 +0000

    [level1][홀짝에 따라 다른 값 반환하기] 제어문

---
+++ Programmers/src/코딩기초트레이닝/홀짝에 따라 다른 값 반환하기.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = 0
+    if n % 2 == 0:
+        for i in range(n+1):
+            if i%2 != 0:
+                continue
+            answer += i ** 2
+    else:
+        for i in range(n+1):
+            if i%2 == 0:
+                continue
+            answer += i
+
+    return answer
+
+
