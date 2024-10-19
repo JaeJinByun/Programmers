ce0fa62bfb4ba7a770376f97549733bed977dd28
변재진 <jj.byun@mobigen.com>
2024-10-19 23:32:00 +0900

    [level2][요격 시스템] 정렬

---
+++ Programmers/src/level2/요격_시스템/Main.java
@@ -0,0 +1,32 @@
+package level2.요격_시스템;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+  }
+}
+
+class Solution {
+
+  public int solution(int[][] targets) {
+    Arrays.sort(targets, Comparator.comparingInt(a -> a[1]));
+
+    int answer = 0;
+
+    int start = 0;
+    int end = 0;
+
+    for (int[] target : targets) {
+      if (target[0] >= end) {
+        answer++;
+        end = target[1];
+      }
+    }
+
+    return answer;
+  }
+}
